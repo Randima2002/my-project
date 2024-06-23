75bbdc866f7ebb8c3a8e640fcf9791860eef2da6
Prabath1122 <128120723+Prabath1122@users.noreply.github.com>
2024-06-23 19:00:18 +0530

    auto run

--- src/app/(component)/Silder.jsx
+++ src/app/(component)/Silder.jsx
@@ -51,12 +51,12 @@ const Silder = () => {
     setSelectedtoDates(formattedtoDateto);
   };
 
-  // const searchAvailability = () =>{
-  //   console.log(selectedfromDates);
-  //   console.log(selectedtoDates);
-  //   console.log(valueadult);
-  //   console.log(valuechild);
-  // }
+  const searchAvailability = () =>{
+    console.log(selectedfromDates);
+    console.log(selectedtoDates);
+    console.log(valueadult);
+    console.log(valuechild);
+  }
 
   useEffect(() => {
     // console.log(selectedfromDates);
@@ -68,7 +68,7 @@ const Silder = () => {
       <Swiper
         spaceBetween={30}
         effect={'fade'}
-        autoplay={false}
+        autoplay={true}
         modules={[Autoplay, EffectFade]}
         className="mySwiper"
         loop={true}
@@ -121,12 +121,138 @@ const Silder = () => {
         </SwiperSlide>
         <SwiperSlide>
           <Image src={banner2} alt="banner" className=' w-full mobile:h-2/3 laptop:h-screen' />
+           <div className=' w-[80%] mx-auto bg-white top-[70%] left-[10%] z-10 absolute'>
+            <div className=' w-full flex flex-row p-4 gap-4' >
+
+              <div className=' flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'>Check in</label>
+                <Stack spacing={10} direction="column" alignItems="flex-start">
+                  <DatePicker format="MMMM dd, yyyy" onChange={(value) => handleDateChangefrom(value)} className=' hover:ring-black'/>
+                </Stack>
+              </div>
+
+              <div className='  flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'>Check Out</label>
+                <Stack spacing={10} direction="column" alignItems="flex-start">
+                  <DatePicker format="MMMM dd, yyyy" onChange={(value) => handleDateChangeto(value)} />
+                </Stack>
+              </div>
+
+              <div className='  flex flex-col' >
+                <label htmlFor="" className=' my-auto leading-5'>Adult</label>
+                <InputGroup>
+                  <InputGroup.Button onClick={handleMinusadult}>-</InputGroup.Button>
+                  <InputNumber className={'custom-input-number'} value={valueadult} onChange={(value) => setvalueadult(value)} />
+                  <InputGroup.Button onClick={handlePlusadult}>+</InputGroup.Button>
+                </InputGroup>
+              </div>
+              
+              <div className='flex flex-col' >
+                <label htmlFor="" className=' my-auto leading-5'>Children</label>
+                <InputGroup>
+                  <InputGroup.Button onClick={handleMinuschild}>-</InputGroup.Button>
+                  <InputNumber className={'custom-input-number'} value={valuechild} onChange={(value) => setvaluechild(value)} />
+                  <InputGroup.Button onClick={handlePluschild}>+</InputGroup.Button>
+                </InputGroup>
+              </div>
+
+              <div className='flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'></label>
+                <Button size="md" className=' my-auto' onClick={searchAvailability}>Check Availability</Button>
+              </div>
+
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <Image src={banner3} alt="banner" className=' w-full mobile:h-2/3 laptop:h-screen' />
+           <div className=' w-[80%] mx-auto bg-white top-[70%] left-[10%] z-10 absolute'>
+            <div className=' w-full flex flex-row p-4 gap-4' >
+
+              <div className=' flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'>Check in</label>
+                <Stack spacing={10} direction="column" alignItems="flex-start">
+                  <DatePicker format="MMMM dd, yyyy" onChange={(value) => handleDateChangefrom(value)} className=' hover:ring-black'/>
+                </Stack>
+              </div>
+
+              <div className='  flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'>Check Out</label>
+                <Stack spacing={10} direction="column" alignItems="flex-start">
+                  <DatePicker format="MMMM dd, yyyy" onChange={(value) => handleDateChangeto(value)} />
+                </Stack>
+              </div>
+
+              <div className='  flex flex-col' >
+                <label htmlFor="" className=' my-auto leading-5'>Adult</label>
+                <InputGroup>
+                  <InputGroup.Button onClick={handleMinusadult}>-</InputGroup.Button>
+                  <InputNumber className={'custom-input-number'} value={valueadult} onChange={(value) => setvalueadult(value)} />
+                  <InputGroup.Button onClick={handlePlusadult}>+</InputGroup.Button>
+                </InputGroup>
+              </div>
+              
+              <div className='flex flex-col' >
+                <label htmlFor="" className=' my-auto leading-5'>Children</label>
+                <InputGroup>
+                  <InputGroup.Button onClick={handleMinuschild}>-</InputGroup.Button>
+                  <InputNumber className={'custom-input-number'} value={valuechild} onChange={(value) => setvaluechild(value)} />
+                  <InputGroup.Button onClick={handlePluschild}>+</InputGroup.Button>
+                </InputGroup>
+              </div>
+
+              <div className='flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'></label>
+                <Button size="md" className=' my-auto' onClick={searchAvailability}>Check Availability</Button>
+              </div>
+
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <Image src={banner4} alt="banner" className=' w-full mobile:h-2/3 laptop:h-screen' />
+           <div className=' w-[80%] mx-auto bg-white top-[70%] left-[10%] z-10 absolute'>
+            <div className=' w-full flex flex-row p-4 gap-4' >
+
+              <div className=' flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'>Check in</label>
+                <Stack spacing={10} direction="column" alignItems="flex-start">
+                  <DatePicker format="MMMM dd, yyyy" onChange={(value) => handleDateChangefrom(value)} className=' hover:ring-black'/>
+                </Stack>
+              </div>
+
+              <div className='  flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'>Check Out</label>
+                <Stack spacing={10} direction="column" alignItems="flex-start">
+                  <DatePicker format="MMMM dd, yyyy" onChange={(value) => handleDateChangeto(value)} />
+                </Stack>
+              </div>
+
+              <div className='  flex flex-col' >
+                <label htmlFor="" className=' my-auto leading-5'>Adult</label>
+                <InputGroup>
+                  <InputGroup.Button onClick={handleMinusadult}>-</InputGroup.Button>
+                  <InputNumber className={'custom-input-number'} value={valueadult} onChange={(value) => setvalueadult(value)} />
+                  <InputGroup.Button onClick={handlePlusadult}>+</InputGroup.Button>
+                </InputGroup>
+              </div>
+              
+              <div className='flex flex-col' >
+                <label htmlFor="" className=' my-auto leading-5'>Children</label>
+                <InputGroup>
+                  <InputGroup.Button onClick={handleMinuschild}>-</InputGroup.Button>
+                  <InputNumber className={'custom-input-number'} value={valuechild} onChange={(value) => setvaluechild(value)} />
+                  <InputGroup.Button onClick={handlePluschild}>+</InputGroup.Button>
+                </InputGroup>
+              </div>
+
+              <div className='flex flex-col'>
+                <label htmlFor="" className=' my-auto leading-5'></label>
+                <Button size="md" className=' my-auto' onClick={searchAvailability}>Check Availability</Button>
+              </div>
+
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
